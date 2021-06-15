c0f5bdffbe21231f6a8039fc7866d9a118ac13ca
DevStar999 <wolverinerage99@gmail.com>
2021-06-15 19:17:31 +0530

    Completed testing of one cell for game piece falling into cell animation

--- app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.gameconnect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     public void makeMove(View view) {
-        // Keeping this empty for now
+        String viewTag = view.getTag().toString();
+        Log.i("Slot Pressed :", "Slot: " + viewTag);
+        ((ImageView) view).setImageResource(R.drawable.red);
+        view.setY(-1*1500);
+        view.animate().translationY(0).setDuration(200);
     }
 
     @Override
