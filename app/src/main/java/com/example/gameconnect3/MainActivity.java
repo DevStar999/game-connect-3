72f8ee55e2c86398e7b0d5a8d6eafa3ef8e12e32
DevStar999 <wolverinerage99@gmail.com>
2021-06-15 20:05:34 +0530

    Added a blank image at start as placeholder for the game pieces and tested all cells for move animation

--- app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -12,9 +12,11 @@ public class MainActivity extends AppCompatActivity {
     public void makeMove(View view) {
         String viewTag = view.getTag().toString();
         Log.i("Slot Pressed :", "Slot: " + viewTag);
-        ((ImageView) view).setImageResource(R.drawable.red);
-        view.setY(-1*1500);
-        view.animate().translationY(0).setDuration(200);
+        //Log.i("Co-ordinates :", "x: " + location[0] + ", y: " + location[1]);
+        ((ImageView) view).setImageResource(R.drawable.red);// Testing with red piece
+        view.setTranslationY(-1000);
+        view.setAlpha(1);
+        view.animate().translationYBy(1000).setDuration(200);
     }
 
     @Override
